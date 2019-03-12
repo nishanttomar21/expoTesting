72be9741b02200859915eb4e06376a621cd771f5
Nishant Tomar <nishant@wayload.co>
2019-03-12 22:59:56 +0530

    added react-native-paper [note: add core-js in package.json]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, Button } from 'react-native-paper';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <PaperProvider>
+          <Button mode="contained">
+            Press Me!
+          </Button>
+        </PaperProvider>
       </View>
     );
   }
